52f897ff466525175cbb9c4c071fd8694429ed77
Andrew An <andyan7@gmail.com>
2022-05-17 20:06:21 -0700

    components, dependencies, Routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Layout from './components/Layout';
+import Home from './components/Home';
+import Calendar from './components/About';
+import Login from './components/Login';
+import Signup from './components/Signup';
+import Plants from './components/Plants';
+import Nav from './components/Nav';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Layout />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Calendar />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/" element={<Signup />} />
+        <Route path="/" element={<Plants />} />
+        <Route path="/" element={<Nav />} />
+      </Routes>
+    </>
   );
 }
 
---
+++ client/src/components/Home/index.js
@@ -0,0 +1,18 @@
+// placeholder for future implementation
+import Calendar from '../../components/Calendar';
+import Header from '../../components/Header';
+import Nav from '../../components/Nav';
+
+const Home = () => {
+    return (
+        <>
+            <Calendar />
+            <Header />
+            <Nav />
+        </>
+    )
+}
+
+
+
+export default Home;
